8fe9565a089a4a5d7870ce5969812b6f26718769
aiyandaodi <529158785@qq.com>
2019-12-28 15:51:46 +0800

    Refactoring

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,7 +1,17 @@
 public class Rover {
+    public static final String EAST = "E";
+    public static final String NORTH = "N";
+    public static final String WEST = "W";
+    public static final String SOUTH = "S";
     private String e;
     private Area area;
     public void land(Area area, int x, int y, String e) {
+        if (x > area.getX()) {
+            throw new IllegalArgumentException("x=" + x + " is out of area width " + area.getX());
+        }
+        if (y > area.getY()) {
+            throw new IllegalArgumentException("y=" + y + " is out of area height " + area.getY());
+        }
         this.e = e;
         area.setX(x);
         area.setY(y);
@@ -9,10 +19,52 @@ public class Rover {
     }
 
     public String getPosition() {
-        return ""+ area.getX() + "" + area.getY() + "" + e;
+        return String.format("%d%d%s", area.getX(), area.getY(), e);
     }
 
     public void move() {
+        if (this.e.equals("E")){
+            area.setX(area.getX() + 1);
+        }
+        if (this.e.equals("W")) {
+            area.setX(area.getX() - 1);
+        }
+        if (this.e.equals("N")) {
+            area.setY(area.getY() + 1);
+        }
+        if (this.e.equals("S")) {
+            area.setY(area.getY() - 1);
+        }
+    }
 
+    public void turnLeft() {
+        if (e.equals(EAST)) {
+            e = NORTH;
+            return;
+        }if (e.equals(NORTH)) {
+            e = WEST;
+            return;
+        }if (e.equals(WEST)) {
+            e = SOUTH;
+            return;
+        }if (e.equals(SOUTH)) {
+            e = EAST;
+            return;
+        }
+    }
+    public void turnRight() {
+        if (e.equals(EAST)) {
+            e = SOUTH;
+            return;
+        }if (e.equals(SOUTH)) {
+            e = WEST;
+            return;
+        }if (e.equals(WEST)) {
+            e = NORTH;
+            return;
+        }if (e.equals(NORTH)) {
+            e = EAST;
+            return;
+        }
     }
 }
