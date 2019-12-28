f668ed12b991dd6cdd6499af7daddfc7d2964166
aiyandaodi <529158785@qq.com>
2019-12-28 14:33:20 +0800

    pass

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,10 +1,15 @@
 public class Rover {
+    private String e;
+    private Area area;
     public void land(Area area, int x, int y, String e) {
-        
+        this.e = e;
+        area.setX(x);
+        area.setY(y);
+        this.area = area;
     }
 
     public String getPosition() {
-        return null;
+        return ""+ area.getX() + "" + area.getY() + "" + e;
     }
 
     public void move() {
